9e04d93348d210427accc6cc17b1cfe2fcabfc15
JagTheFriend <JagTheFriend12@gmail.com>
2025-06-24 15:16:03 +0530

    Add prod API url
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- frontend/src/config/env.ts
+++ frontend/src/config/env.ts
@@ -1,3 +1,5 @@
 export const API_CONFIG = {
-  BASE_URL: import.meta.env.VITE_API_BASE_URL,
+  BASE_URL:
+    import.meta.env.VITE_API_BASE_URL ??
+    "https://outflo-assignment-backend-iygh.onrender.com",
 } as const;
